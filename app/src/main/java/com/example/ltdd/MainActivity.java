cc38bdddefe07f2adb6b90be779d2e9c23b0f214
baoquoc <baoquoc536@gmail.com>
2023-10-05 16:35:35 +0700

    hello

---
+++ app/src/main/java/com/example/ltdd/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ltdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   Button bntClick,bntThoat;
+   TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
